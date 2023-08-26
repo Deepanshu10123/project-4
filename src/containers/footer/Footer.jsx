1441134a90e4cb4a983ae48398ef09371ad671eb
Deepanshu <76985826+Deepanshu10123@users.noreply.github.com>
2023-08-26 09:09:17 +0530

    Add files via upload

---
+++ src/containers/footer/Footer.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import './footer.css' 
+import gpt3Logo from '../../assets/logo.svg'
+
+const Footer = () => {
+  return (
+    <div className='gpt3__footer section__padding'>
+      <div className='gpt3__footer-heading'>
+        <h1 className='gradient__text'>Do you want to step in to the future before others</h1>
+      </div>
+      <div className='gpt3__footer-btn'>
+        <p>Request Early Access</p>
+      </div>
+
+      <div className='gpt3__footer-links'>
+        <div className='gpt3__footer-links_logo'>
+          <img src={gpt3Logo} alt='logo'/>
+          <p>Full-Stack Developer, All Rights Reserved</p>
+        </div>
+        <div className='gpt3__footer-links_div'>
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className='gpt3__footer-links_div'>
+          <h4>Comapny</h4>
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+        <div className='gpt3__footer-links_div'>
+          <h4>Get in touch</h4>
+          <p>Najafgarh New Delhi-43</p>
+          <p>9267951774</p>
+          <p>Deepanshu10123@gmail.com</p>
+        </div>
+      </div>
+      <div className='gpt3__footer-copyright'>
+        <p>© 2023 GPT-3. All rights reserved to Deepanshu.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
